268bf066897aefd86fcad3813ba23428da219b76
candidp3 <83573137+candidp3@users.noreply.github.com>
2026-09-14 15:42:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='routine-check-phase6-7-v1';
+const CACHE_NAME='routine-check-phase6-8-v1';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
